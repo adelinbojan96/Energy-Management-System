4432195312b4e8146a2b3f7aa8815c46293e31cb
adelinbojan96 <adelinbojan96@gmail.com>
2025-10-28 22:01:08 +0200

    final

---
+++ api_microservice/src/main/java/com/example/demo/security/JwtUtil.java
@@ -0,0 +1,37 @@
+package com.example.demo.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+public class JwtUtil {
+    private static final String SECRET_KEY = "mysecretkey12345678901234567890123456789012"; // use same as in AuthService
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSigningKey())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Claims getClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
